04f5a14a5c88acdb67b0ac0e75a4656c91034e21
Hex <35177464+HexCodeFFF@users.noreply.github.com>
2021-12-04 00:12:33 -0600

    epic pascal but Slow Lol

--- scene.py
+++ scene.py
@@ -1,8 +1,35 @@
 from manim import *
 
 
-class Scene(Scene):
+def pascal_row(n):
+    line = [1]
+    for k in range(n):
+        line.append(line[k] * (n - k) / (k + 1))
+    return line
+
+
+def only_numeric_subobjects(mobj: MathTex) -> List[SingleStringMathTex]:
+    return [m for m in mobj.submobjects if m.get_tex_string().strip().isnumeric()]
+
+
+class Scene(MovingCameraScene):
     def construct(self):
-        tex = Text(r"\LaTeX", font_size=144)
+        tex = MathTex("{{1}}", font_size=64).to_edge(UP)
         self.add(tex)
-        self.wait(1)
+        all_mobjects = [tex]
+        self.camera.frame.match_height(tex)
+        self.camera.frame.rescale_to_fit(tex.length_over_dim(1)+1, 1)
+        self.camera.frame.move_to(tex)
+        self.play(Write(tex))
+        for i in range(1, 20):
+            newtex = MathTex(r" \enspace ".join([f'{{{{{j:g}}}}}' for j in pascal_row(i)]), font_size=64)\
+                .next_to(tex, DOWN)
+            all_mobjects.append(newtex)
+            transforms = []
+            onsnewtex = only_numeric_subobjects(newtex)
+            for j, mobj in enumerate(only_numeric_subobjects(tex)):
+                transforms += [Transform(mobj.copy(), onsnewtex[j].copy()),
+                               Transform(mobj.copy(), onsnewtex[j + 1].copy())]
+            print([m for m in tex.submobjects if m.get_tex_string().strip().isnumeric()])
+            self.play(*transforms, self.camera.auto_zoom(all_mobjects, 1))
+            tex = newtex
